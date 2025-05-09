9c3636b1ab2fe23e4fa8f70ee4e11c497c140806
Miguel Jódar <alumno23civica@gmail.com>
2025-05-09 11:45:36 +0000

    tabla intermedia order_items

--- models/staging/sql_server_dbo/stg_sql_server__order_items.sql
+++ models/staging/sql_server_dbo/stg_sql_server__order_items.sql
@@ -8,8 +8,8 @@ renamed as (
 
     select
         {{ dbt_utils.generate_surrogate_key(['order_id', 'product_id']) }} as order_item_id,
-        order_id,
-        product_id,
+        {{ dbt_utils.generate_surrogate_key(['order_id']) }} as order_sk,
+        {{ dbt_utils.generate_surrogate_key(["product_id"]) }} as product_sk,
         cast(quantity as integer) as quantity,
         _fivetran_deleted as is_deleted,
         _fivetran_synced as at_synced
